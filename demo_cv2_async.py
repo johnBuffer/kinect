a8b32cf095d72b087c51e4a9d93497b61c2923d2
johnBuffer <jean.tampon@etu.univ-nantes.fr>
2017-03-03 16:29:25 +0100

    added text

--- demo_cv2_async.py
+++ demo_cv2_async.py
@@ -31,6 +31,7 @@ def display_rgb(dev, data, timestamp):
     t_start = datetime.datetime.now()
     global keep_running
 
+
     result = draw_canny(data)
     #data = frame_convert2.video_cv(data)
     #result = data.copy()
@@ -38,7 +39,7 @@ def display_rgb(dev, data, timestamp):
     t_end = datetime.datetime.now()
     t_delta = t_end - t_start
 
-    cv2.putText(result, str(1000000/t_delta.microseconds), (0, 25), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
+    cv2.putText(result, str(1000000/t_delta.microseconds), (0, 25), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
     cv2.imshow('RGB', result)
     
     if cv2.waitKey(10) == 27:
