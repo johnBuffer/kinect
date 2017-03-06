9d8db09f3639016c848ae9fad5a1343ed454a116
johnBuffer <jean.tampon@etu.univ-nantes.fr>
2017-03-06 11:09:45 +0100

    test fps

--- demo_cv2_async.py
+++ demo_cv2_async.py
@@ -5,7 +5,7 @@ import frame_convert2
 import numpy as np
 import datetime
 
-#cv2.namedWindow('Depth')
+cv2.namedWindow('Depth')
 cv2.namedWindow('RGB')
 keep_running = True
 
@@ -18,10 +18,14 @@ def draw_canny(data):
 
 def display_depth(dev, data, timestamp):
     global keep_running
+    t_start = datetime.datetime.now()
     data2 = frame_convert2.pretty_depth_cv(cv2.resize(data, (0, 0), fx=0.25, fy=0.25))
     #data2 = frame_convert2.pretty_depth_cv(data)
 
     #data2 = cv2.applyColorMap(data.astype(np.uint8), cv2.COLORMAP_JET)
+    t_end = datetime.datetime.now()
+    t_delta = t_end - t_start
+    cv2.putText(result, str(1000000/t_delta.microseconds), (0, 25), cv2.FONT_HERSHEY_PLAIN, 2, (127, 0, 0), 2)
     cv2.imshow('Depth', cv2.resize(data2, (0, 0), fx=4, fy=4))
     if cv2.waitKey(10) == 27:
         keep_running = False
@@ -52,9 +56,9 @@ def body(*args):
 
 
 print('Press ESC in window to stop')
-"""freenect.runloop(depth=display_depth,
+freenect.runloop(depth=display_depth,
                  video=None,
-                 body=body)"""
+                 body=body)
 
 freenect.runloop(depth=None,
                  video=display_rgb,
